4dec7d1de6dd7cd547816a8a2fe4ab46201890de
Soundwave0 <konradkosuge@gmail.com>
2025-11-06 00:09:05 +0100

    add libs

---
+++ lib/SOMDriver/SOMDriver.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <driver/gpio.h>
+#include <esp_log.h>
+
+#include "HX711.h"
+
+int HX711_config(){
+    return -1;
+}
---
+++ lib/SOMDriver/SOMDriver.h
@@ -0,0 +1,7 @@
+#ifndef SOMDriver_h
+#define SOMDriver_h
+#include "HX711.h"
+//configures the HX711 hardware parameters and initializes the readings
+int HX711_config();
+
+#endif
